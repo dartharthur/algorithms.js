ad6121918c9389560c1cc9057f4a04d613e39466
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-02-07 17:24:32 -0800

    [solved] eventing library

--- toy-problem-collection/collection/js-fundamentals.js
+++ toy-problem-collection/collection/js-fundamentals.js
@@ -18,7 +18,6 @@ Function.prototype.bind = function(context) {
 const compose = function() {
   let functions = Array.from(arguments);
   return function(arg) {
-    // let initialArgs = Array.from(arguments);
     return functions.reduceRight(function(input, func) {
       return func.call(null, input);
     }, arg);
@@ -28,11 +27,52 @@ const compose = function() {
 const pipe = function() {
   let functions = Array.from(arguments);
   return function(arg) {
-    // let initialArgs = Array.from(arguments);
     return functions.reduce(function(input, func) {
       return func.call(null, input);
     }, arg);
   };
 };
 
-const compose = (...args) => v => args.reduceRight((a, f) => f(a), v);  
+/** 
+ * eventing system
+ * note: explain to students that you can check the console in the challenge app
+ */
+/** version 1 (explicit) */
+var mixEventsV1 = (obj) => {
+  var events = {};
+
+  obj.trigger = function(event) {
+    var boundArguments = Array.prototype.slice.call(arguments, 1);
+    var eventHandlers = events[event];
+    if (eventHandlers !== undefined) {
+      for (var i = 0; i < eventHandlers.length; i++) {
+        eventHandlers[i].apply(this, boundArguments);
+      }
+    }
+  };
+
+  obj.on = function(event, callback) {
+    var eventHandlers = events[event];
+    if (eventHandlers !== undefined) {
+      eventHandlers.push(callback);
+    } else {
+      events[event] = []
+      events[event].push(callback);
+    }
+  };
+  return obj;
+};
+
+/** version 2 (succinct) */
+const mixEventsV2 = (obj) => {
+  const events = {};
+
+  obj.trigger = function(event, ...args) {
+    events[event] && events[event].forEach(event => event.apply(this, args));
+  };
+
+  obj.on = function(event, callback) {
+    events[event] ? events[event].push(callback) : events[event] = [callback];
+  };
+  return obj;
+};
