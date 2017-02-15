b5c9133d67030542a418cc9bdfa14d53ad559d38
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-02-15 12:24:15 -0800

    [solved] insertion sort

--- toy-problem-collection/collection/sorting-algorithms.js
+++ toy-problem-collection/collection/sorting-algorithms.js
@@ -3,7 +3,7 @@ function bubbleSort(array) {
 
   while (flag) {
     flag = false;
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       if (array[i] > array[i + 1]) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]];
         flag = true;
@@ -11,5 +11,30 @@ function bubbleSort(array) {
     }
   }
 
+  return array;
+};
+
+function insertionSortV2(array) {
+  for (let i = 0 ; i < array.length; i++) {
+    if (array[i + 1] && array[i].value > array[i + 1].value) {
+      for (let j = i + 1; j >= 0; j--) {
+        if (array[j - 1] && array[j].value < array[j - 1].value) {
+          [array[j - 1], array[j]] = [array[j], array[j - 1]];
+        } else {
+          break;
+        }
+      }
+    }
+  }
+  return array;
+};
+
+function insertionSortV2(array) {
+  for (let i = 1; i < array.length; i++) {
+    /** key point is we can bake our conditional logic into second element of for loop */
+    for (let j = i; j > 0 && array[j].value < array[j-1].value; j--) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
+    }
+  }
   return array;
 };
