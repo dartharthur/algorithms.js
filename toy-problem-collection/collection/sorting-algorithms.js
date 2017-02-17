55472b038481e9c368f14262dffe697ff314ceb9
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-02-16 16:36:20 -0800

    [solved] selection sort

--- toy-problem-collection/collection/sorting-algorithms.js
+++ toy-problem-collection/collection/sorting-algorithms.js
@@ -1,6 +1,5 @@
 function bubbleSort(array) {
   let flag = true;
-
   while (flag) {
     flag = false;
     for (let i = 0; i < array.length; i++) {
@@ -10,7 +9,6 @@ function bubbleSort(array) {
       }
     }
   }
-
   return array;
 };
 
@@ -35,6 +33,17 @@ function insertionSortV2(array) {
     for (let j = i; j > 0 && array[j].value < array[j-1].value; j--) {
       [array[j - 1], array[j]] = [array[j], array[j - 1]];
     }
+  }
+  return array;
+};
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j;
+    }
+    if (min > i) [array[i], array[min]] = [array[min], array[i]];
   }
   return array;
 };
