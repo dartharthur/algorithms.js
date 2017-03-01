bac01532d51099ceb3cef50558758cc4d9c1ece3
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-03-01 15:45:35 -0800

    [update] added more solutions to paths to work

--- toy-problem-collection/dynamic-programming.js
+++ toy-problem-collection/dynamic-programming.js
@@ -1,33 +1,14 @@
-/** bottom up - O(n) Time & O(1) Space */
-function climbStairsV1(n) {
-  if (n === 0 || n === 1 || n === 2) return n;
-
-  let a = 1;
-  let b = 2;
-  let solutionCount = 0;
-
-  for (let i = 2; i < n; i++) {
-    solutionCount = a + b;
-    a = b;
-    b = solutionCount;
+/** recursive solution */
+function rClimbStairs(n) {
+  if (n === 0 || n === 1 || n === 2) {
+    return n;
+  } else {
+    return rClimbStairs(n - 1) + rClimbStairs(n - 2);
   }
-
-  return solutionCount;
-};
-
-/** bottom up - O(n) Time & O(n) Space */
-function climbStairsV2(n) {
-  let solutions = [0, 1, 2];
-
-  for (let i = 3; i <= n; i++) {
-    solutions[i] = solutions[i - 1] + solutions[i - 2];
-  }
-
-  return solutions[n];
 };
 
 /** top down - O(n) Time & O(n) Space */
-const climbStairsV3 = (function() {
+const tdClimbStairs = (function() {
   const memo = {};
   function f(n) {
     let solutionCount;
@@ -51,6 +32,34 @@ const climbStairsV3 = (function() {
 // actual output: 8
 // console.log(climbStairsV3(5));
 
+/** bottom up - O(n) Time & O(1) Space */
+function buClimbStairsV1(n) {
+  if (n === 0 || n === 1 || n === 2) return n;
+
+  let a = 1;
+  let b = 2;
+  let solutionCount = 0;
+
+  for (let i = 2; i < n; i++) {
+    solutionCount = a + b;
+    a = b;
+    b = solutionCount;
+  }
+
+  return solutionCount;
+};
+
+/** bottom up - O(n) Time & O(n) Space */
+function buClimbStairsV2(n) {
+  let solutions = [0, 1, 2];
+
+  for (let i = 3; i <= n; i++) {
+    solutions[i] = solutions[i - 1] + solutions[i - 2];
+  }
+
+  return solutions[n];
+};
+
 /** recursive solution */
 function rPathsToWork(e, s) {
   let solutionCount = 0;
@@ -59,23 +68,22 @@ function rPathsToWork(e, s) {
   return solutionCount;
 };
 
-/** top-down (memoization) */
-const tdPathsToWork = (function() {
+/** top down (memoization) */
+const tdPathsToWorkV1 = (function() {
   const memo = {};
   function f(e, s) {
     let solutionCount;
-    if (memo[JSON.stringify([e,s])]) {
-      solutionCount = memo[JSON.stringify([e,s])];
+    if (memo[e + ',' + s]) {
+      solutionCount = memo[e + ',' + s];
     } else {
       if (e === 0 || s === 0) {
-        memo[JSON.stringify([e,s])] = 1;
-        memo[JSON.stringify([s,e])] = 1;
+        memo[e + ',' + s] = 1;
+        memo[s + ',' + e] = 1;
       } else {
-        memo[JSON.stringify([e,s])] = f(e - 1, s) + f(e, s - 1);
+        memo[e + ',' + s] = f(e - 1, s) + f(e, s - 1);
       }
-      solutionCount = memo[JSON.stringify([e,s])];
+      solutionCount = memo[e + ',' + s];
     }
-    // console.log('here is our cache', memo);
     return solutionCount;
   }
   return f;
@@ -83,4 +91,34 @@ const tdPathsToWork = (function() {
 
 // expected output: 6
 // actual output: 6
-console.log(tdPathsToWork(2,2));
+// console.log(tdPathsToWork(2,2));
+
+/** top down (memoization) - concise sample code */
+function tdPathsToWorkV2(e, s) {
+ const cache = [[0]];
+ for (let i = 1; i <= s; i++) {
+   cache.push([1]);
+ }
+ for (let j = 1; j <= e; j++) {
+   cache[0].push(1);
+ }
+ const r = (e, s) => {
+   if (cache[e][s]) return cache[e][s];
+   return cache[e][s] = r(e - 1, s) + r(e, s - 1);
+ }
+ return r(e, s);
+};
+
+/** bottom up */
+function buPathsToWorks(e, s) {
+  const map = [];
+  for (let i = 0; i <= s; i++) {
+    let row = [];
+    for (let j = 0; j <= e; j++) {
+      if (i === 0 || j === 0) row.push(1);
+      else row.push(map[i - 1][j] + row[j - 1]);
+    }
+    map.push(row);
+  }
+  return map[s][e];
+};
