6468b05cbf450be4334c204a98410dfd7679364c
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-04-04 15:57:20 -0700

    [solved] rotate matrix

--- toy-problem-collection/matrices.js
+++ toy-problem-collection/matrices.js
@@ -17,3 +17,31 @@ function multiplicationMatrixMap(factors) {
     });
   });
 }
+
+/** 
+ * rotate NxN matrix 90 degrees
+ * @param {array} matrix
+ * @param {number} dir - direction to rotate (-1 counter-clockwise, 1 clockwise)
+ */
+function rotateMatrix(matrix, dir = 1) {
+  let rotatedMatrix = [];
+  if (dir === -1) {
+    for (let j = matrix.length - 1; j >= 0; j--) {
+      let row = [];
+      for (let i = 0; i < matrix.length; i++) {
+        row.push(matrix[i][j]);
+      }
+      rotatedMatrix.push(row);
+    }
+    return rotatedMatrix;
+  } else {
+    for (let j = 0; j < matrix.length; j++) {
+      let row = [];
+      for (let i = matrix.length - 1; i >= 0; i--) {
+        row.push(matrix[i][j]);
+      }
+      rotatedMatrix.push(row);
+    }
+    return rotatedMatrix;
+  }
+}
