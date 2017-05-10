8f5da3fbaf04ab00ed11f414a35b6976af9a8df8
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-05-09 22:29:11 -0700

    [solved] move zeroes

--- toy-problem-collection/arrays.js
+++ toy-problem-collection/arrays.js
@@ -145,3 +145,13 @@ function uniqueAndFlatten(array) {
 
   return uniqueArray;
 }
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums) {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] === 0) nums.push(nums.splice(i, 1)[0]);
+    }
+}
