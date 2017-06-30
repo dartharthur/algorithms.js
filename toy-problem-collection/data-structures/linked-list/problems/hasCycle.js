d53b8dd5ff4a64fd3c79b13a9a9bd1cc084efb13
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-06-30 14:21:38 -0700

    [solved] added solution to hasCycle (linked-list)

--- toy-problem-collection/data-structures/linked-list/problems/hasCycle.js
+++ toy-problem-collection/data-structures/linked-list/problems/hasCycle.js
@@ -1,6 +1,36 @@
-const hasCyle = linkedList => {
-  let trailer = linkedList;
-  let runner = linkedList;
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycleS1 = head => {
+  const nodesSeen = new Set();
+  while (head !== null) {
+    if (nodesSeen.has(head)) {
+      return true;
+    } else {
+      nodesSeen.add(head)
+    }
+    head = head.next;
+  }
+  return false;
+};
+
+/**
+ * Time Complexity: O(n + k) where k is at most length of cycle
+ * Space Complexity: O(1)
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCyleS2 = head => {
+  if (head === null || head.next === null) return false;
+  let trailer = head;
+  let runner = head;
   while (trailer.next && runner.next && runner.next.next) {
     trailer = trailer.next;
     runner = runner.next.next;
