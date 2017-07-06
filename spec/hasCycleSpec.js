f8f4137cfa00bf485901df4d5bcc910bd9052590
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-07-06 15:20:29 -0700

    [refactor] implemented Jest

--- spec/hasCycleSpec.js
+++
@@ -1,8 +0,0 @@
-const jasmine = require('jasmine');
-const cycles = require('../toy-problem-collection/data-structures/linked-list/problems/hasCycle');
-
-jasmine.describe('a suite', () => {
-  jasmine.it('contains spec with an expectation', () => {
-    jasmine.expect(typeof cycles.hasCycleS2).toBe('function');
-  });
-});
---
+++ toy-problem-collection/data-structures/binary-tree/spec/mergeBinaryTrees.spec.js
@@ -0,0 +1,5 @@
+import mergeBinaryTrees from '../problems/mergeBinaryTrees';
+
+test('it should return hello', () => {
+  expect(mergeBinaryTrees()).toBe('hello');
+});
