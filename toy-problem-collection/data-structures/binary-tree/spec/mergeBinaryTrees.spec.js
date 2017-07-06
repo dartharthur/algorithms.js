7b30e83108e8f3896bd7f11a897e5d1ae910ff0d
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-07-06 15:46:36 -0700

    [test] spec written for mergeBinaryTrees

--- toy-problem-collection/data-structures/binary-tree/spec/mergeBinaryTrees.spec.js
+++ toy-problem-collection/data-structures/binary-tree/spec/mergeBinaryTrees.spec.js
@@ -1,5 +1,24 @@
+import BinaryTree from '../implementation';
 import mergeBinaryTrees from '../problems/mergeBinaryTrees';
 
-test('it should return hello', () => {
-  expect(mergeBinaryTrees()).toBe('hello');
+const tree1 = new BinaryTree(1);
+tree1.left = new BinaryTree(3);
+tree1.right = new BinaryTree(2);
+tree1.left.left = new BinaryTree(5);
+
+const tree2 = new BinaryTree(2);
+tree2.left = new BinaryTree(1);
+tree2.right = new BinaryTree(3);
+tree2.left.right = new BinaryTree(4);
+tree2.right.right = new BinaryTree(7);
+
+const mergedTree = new BinaryTree(3);
+mergedTree.left = new BinaryTree(4);
+mergedTree.right = new BinaryTree(5);
+mergedTree.left.left = new BinaryTree(5);
+mergedTree.left.right = new BinaryTree(4);
+mergedTree.right.right = new BinaryTree(7);
+
+test('it should return a merged binary tree', () => {
+  expect(tree1).toEqual(mergedTree);
 });
