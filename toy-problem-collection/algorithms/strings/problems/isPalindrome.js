b18bf953ddccbdc4a8d7cd3f3aa8505cb62dbceb
Kevin Arthur <kevin.arthur.roy@gmail.com>
2017-07-09 16:10:36 -0700

    [solved] isPalindrome working for more complex strings

--- toy-problem-collection/algorithms/strings/problems/isPalindrome.js
+++ toy-problem-collection/algorithms/strings/problems/isPalindrome.js
@@ -3,9 +3,23 @@
  * @return {boolean}
  */
 
+const sanitizeString = (str) => {
+  let built = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (
+      str.toLowerCase().charCodeAt(i) >= 48 && str.toLowerCase().charCodeAt(i) <= 57
+    ) built += str[i];
+    if (
+      str.toLowerCase().charCodeAt(i) >= 97 && str.toLowerCase().charCodeAt(i) <= 122
+    ) built += str[i].toLowerCase();
+  }
+  return built;
+};
+
 const isPalindrome = (str) => {
-  for (let i = 0; i < Math.floor(str.length / 2); i += 1) {
-    if (str[i] !== str[str.length - (i - 1)]) return false;
+  const sanitizedString = sanitizeString(str);
+  for (let i = 0; i < Math.floor(sanitizedString.length / 2); i += 1) {
+    if (sanitizedString[i] !== sanitizedString[sanitizedString.length - i - 1]) return false;
   }
   return true;
 };
--- toy-problem-collection/algorithms/strings/spec/isPalindrome.spec.js
+++ toy-problem-collection/algorithms/strings/spec/isPalindrome.spec.js
@@ -3,6 +3,8 @@ import isPalindrome from '../problems/isPalindrome';
 const firstString = 'amanaplanacanalpanama';
 const secondString = 'racecar';
 const thirdString = 'raceacar';
+const fourthString = 'A man, a plan, a canal: Panama';
+const fifthString = 'race a car';
 
 describe('An implementation of isPalindrome', () => {
   test('should determine whether a given input string is a palindrome.', () => {
@@ -13,5 +15,11 @@ describe('An implementation of isPalindrome', () => {
   });
   test('should determine whether a given input string is a palindrome.', () => {
     expect(isPalindrome(thirdString)).toBe(false);
+  });
+  test('should determine whether a given input string is a palindrome.', () => {
+    expect(isPalindrome(fourthString)).toBe(true);
+  });
+  test('should determine whether a given input string is a palindrome.', () => {
+    expect(isPalindrome(fifthString)).toBe(false);
   });
 });
