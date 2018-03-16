7c776fa76e150e85374c8b35aeb566d62a39f0df
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-03-15 17:44:30 -0700

    [test] adds spec for QueueStack

---
+++ toy-problem-collection/data-structures/stack/spec/QueueStack.spec.js
@@ -0,0 +1,18 @@
+import QueueStack from "../problems/QueueStack";
+
+describe("A QueueStack", () => {
+  test("should enqueue a value in the correct order.", () => {
+    const queueStack = new QueueStack();
+    queueStack.enqueue(5);
+    queueStack.enqueue(8);
+    const a = queueStack.dequeue();
+    expect(a).toBe(5);
+  });
+  test("should return the correct size.", () => {
+    const queueStack = new QueueStack();
+    queueStack.enqueue(5);
+    queueStack.enqueue(8);
+    queueStack.enqueue(15);
+    expect(queueStack.size()).toBe(3);
+  });
+});
